2ecd13e8be588c727198a61138c965643c70d7f3
Qyoh7 <speedrun7760@gmail.com>
2025-12-09 21:53:01 -0800

    sync

--- final/main.cpp
+++ final/main.cpp
@@ -1,6 +1,5 @@
 #include <cctype>
 #include <iostream>
-#include <string.h>
 #include <vector>
 using namespace std;
 
@@ -8,6 +7,7 @@ int main()
 {
     vector<char> answerKey{'a', 'd', 'b', 'b', 'c', 'b', 'a', 'b', 'c', 'd', 'a', 'c', 'd', 'b', 'd', 'c', 'c', 'a', 'd', 'b'};
     vector<char> userAnswers;
+    vector<int> incorrectAnswers;
     char ans; 
     int incorrect = 0;
     
@@ -28,5 +28,15 @@ int main()
     for (int i = 0; i < userAnswers.size(); i++)
     {
         if (userAnswers.at(i) != ans)
+        {
+            incorrect++;
+            incorrectAnswers.push_back(i);
+        }
+    }
+    cout << "Score: " << answerKey.size() - incorrect << " / " << answerKey.size() << "\n";
+    cout << "Incorrect Answers: \n";
+    for (int ans : incorrectAnswers)
+    {
+        cout << ans << "\n";
     }
 }
